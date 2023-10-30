f746c40819aba6de24a7c3e5353a7a3843d20ea2
Marmik kumar <marmik.kumar.ece23@heritageit.edu.in>
2023-10-31 00:00:17 +0530

    completed about page

--- frontend/src/components/about.js
+++ frontend/src/components/about.js
@@ -8,6 +8,7 @@ const About = () => {
 
   const [choose1,setchoose1] =useState(true);
   const [choose2,setchoose2] =useState(false);
+  const [selectedImage, setSelectedImage] = useState(Myphoto);
 
 
   const handleclick1=()=>{
@@ -20,30 +21,46 @@ const About = () => {
     setchoose1(false);
   }
 
+  
+  
+    const handleImageChange = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        // You can perform additional checks here, such as verifying file type or size
+  
+        // To display a preview of the selected image
+        const reader = new FileReader();
+        reader.onload = () => {
+          setSelectedImage(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+
 
   return (
     <div className='aboutpage'>
       <Nav />
       <div className='aboutbox'>
         <div className='aboutimg'>
-          <img src={Myphoto} alt='img'></img>
+          <img src={selectedImage} alt='img'></img>
+          <input type="file" accept="image/*" onChange={handleImageChange} />
           <div classname="aboutoption">
             <p>Work Link</p>
             <ul className='underline'>
-              <li>Youtube</li>
-              <li>Instagram</li>
               <li>Github</li>
               <li>Linkedin</li>
               <li>Gfg</li>
               <li>Codechef</li>
               <li>Leetcode</li>
+              <li>Resume</li>
             </ul>
           </div>
         </div>
         <div className='aboutcontent'>
           <div className='aboutright'>
             <div className='aboutintro'>
-              <h3>Marmik Kumar</h3>
+              <h2 className='myname'>Marmik Kumar</h2>
               <p>Web Developer</p>
               <p>Ranking:1/10</p>
             </div>
@@ -55,7 +72,7 @@ const About = () => {
 
             <div className="aboutme">
               <div  className='abouthead'><spam onClick={handleclick1} className={choose1 ? "spam":""}>About  </spam> <spam onClick={handleclick2}className={choose2 ? "spam":""}>  Timeline</spam></div>
-              <div className='profile'>
+              <div className={choose1 ? 'profile':"not"}>
                 <ul className='profiletitle'>
                   <li>userId</li>
                   <li>Name</li>
@@ -71,6 +88,23 @@ const About = () => {
                   <li>IT</li>
                 </ul>
               </div>
+
+              <div className={choose2 ? 'profile': "not"}>
+                <ul className='profiletitle'>
+                  <li>Experience</li>
+                  <li>Hourly Rate</li>
+                  <li>Total Project</li>
+                  <li>English Experience</li>
+                  <li>Availability</li>
+                </ul>
+                <ul className='profiledetail'>
+                  <li>1</li>
+                  <li>20$</li>
+                  <li>4</li>
+                  <li>Very Good</li>
+                  <li>6 month</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -79,4 +113,4 @@ const About = () => {
   )
 }
 
-export default About ;
+export default About 
